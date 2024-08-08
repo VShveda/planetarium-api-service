4404cdea1612ef600c74baf5eab24fac6f566772
Viktor Sveda <vinix.ru@gmail.com>
2024-08-08 19:10:34 +0300

    fix ShowTheme model

--- planetarium/models.py
+++ planetarium/models.py
@@ -4,7 +4,7 @@ from user.models import User
 
 
 class ShowTheme(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
