cc37111e86dca3b2918cc85e68f73caf5e0cb7af
Viktor Sveda <vinix.ru@gmail.com>
2024-08-08 18:34:33 +0300

    add PlanetariumDome to planetarium models.py

--- planetarium/models.py
+++ planetarium/models.py
@@ -12,3 +12,16 @@ class AstronomyShow(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     show_theme = models.ManyToManyField(ShowTheme)
+
+
+class PlanetariumDome(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
