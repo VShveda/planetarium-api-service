6ca7d2426f703e2dc79ab7f3f66c4ec71571eb22
Viktor Sveda <vinix.ru@gmail.com>
2024-08-09 15:57:25 +0300

    add url to ShowTheme view end upgraded requirements.txt

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from planetarium.views import ShowThemeViewSet
+
+router = routers.DefaultRouter()
+router.register("show_theme", ShowThemeViewSet)
 
 
 app_name = "planetarium"
 urlpatterns = [
-
+    path("", include(router.urls)),
 ]
