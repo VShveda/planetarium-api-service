1023ff7a2806cf81a1364c279ed0a5350baecd54
Viktor Sveda <vinix.ru@gmail.com>
2024-08-09 15:32:02 +0300

    add ShowThemeViewSet

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from planetarium.models import ShowTheme
+from planetarium.permissions import IsAdminOrIfAuthenticatedReadOnly
+from planetarium.serializers import ShowThemeSerializer
+
+
+class ShowThemeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
