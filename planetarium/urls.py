00c94f4508e7f5b369327d359eaf4e4d25f86123
Viktor Sveda <vinix.ru@gmail.com>
2024-08-09 15:57:25 +0300

    add PlanetariumDomeViewSet end url

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from planetarium.views import (
+    ShowThemeViewSet,
+    AstronomyShowViewSet,
+    PlanetariumDomeViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("show_theme", ShowThemeViewSet)
+router.register("astronomy_show", AstronomyShowViewSet)
+router.register("planetarium_dome", PlanetariumDomeViewSet)
 
 
 app_name = "planetarium"
 urlpatterns = [
-
+    path("", include(router.urls)),
 ]
--- planetarium/views.py
+++ planetarium/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework import mixins, viewsets
 
-from planetarium.models import ShowTheme
+from planetarium.models import ShowTheme, AstronomyShow, PlanetariumDome
 from planetarium.permissions import IsAdminOrIfAuthenticatedReadOnly
-from planetarium.serializers import ShowThemeSerializer
+from planetarium.serializers import ShowThemeSerializer, AstronomyShowSerializer, PlanetariumDomeSerializer
 
 
 class ShowThemeViewSet(
@@ -13,4 +13,24 @@ class ShowThemeViewSet(
 ):
     queryset = ShowTheme.objects.all()
     serializer_class = ShowThemeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class AstronomyShowViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class PlanetariumDomeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
