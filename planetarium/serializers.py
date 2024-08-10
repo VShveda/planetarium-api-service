d9dd59c331926e105bf623250eec415e12d7e71f
Viktor Sveda <vinix.ru@gmail.com>
2024-08-10 18:31:07 +0300

    add create func to ReservationSerializer

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from django.db import transaction
 from rest_framework import serializers
 
 from planetarium.models import (
@@ -87,9 +88,19 @@ class PlanetariumDomeSerializer(serializers.ModelSerializer):
 
 
 class ReservationSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=True, allow_empty=False)
+
     class Meta:
         model = Reservation
         fields = ("id", "created_at", "user")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            reservation = Reservation.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(reservation=reservation, **ticket_data)
+            return reservation
 
 
 class ReservationListSerializer(ReservationSerializer):
