0558e266faa992bc28b549b07548d428c957b6b4
Viktor Sveda <vinix.ru@gmail.com>
2024-08-10 15:43:37 +0300

    add _params_to_ints, get_queryset, get_serializer_class, upload_image to AstronomyShowViewSet

--- planetarium/views.py
+++ planetarium/views.py
@@ -3,8 +3,10 @@ from datetime import datetime
 from django.db.models import Count, F
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema, OpenApiParameter
-from rest_framework import mixins, viewsets, pagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import mixins, viewsets, pagination, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
 from planetarium.permissions import IsAdminOrIfAuthenticatedReadOnly
 from planetarium.models import (
@@ -21,7 +23,7 @@ from planetarium.serializers import (
     PlanetariumDomeSerializer,
     ReservationSerializer,
     ReservationListSerializer,
-    ShowSessionSerializer,
+    ShowSessionSerializer, AstronomyShowListSerializer, AstronomyShowDetailSerializer, AstronomyShowImageSerializer,
 )
 
 
@@ -40,9 +42,69 @@ class AstronomyShowViewSet(
     mixins.ListModelMixin,
     viewsets.GenericViewSet
 ):
-    queryset = AstronomyShow.objects.all()
+    queryset = AstronomyShow.objects.prefetch_related("show_theme")
     serializer_class = AstronomyShowSerializer
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        title = self.request.query_params.get("title")
+        show_theme = self.request.query_params.get("show_theme")
+        queryset = self.queryset
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if show_theme:
+            show_theme_ids = self._params_to_ints(show_theme)
+            queryset = queryset.filter(show_theme__id__in=show_theme_ids)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+        if self.action == "upload_image":
+            return AstronomyShowImageSerializer
+        return AstronomyShowSerializer
+
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser]
+    )
+    def upload_image(self, request, pk=None):
+        astronomy_show = self.get_object()
+        serializer = self.get_serializer(astronomy_show, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "title",
+                type=OpenApiTypes.STR,
+                description="Filter by title of the show",
+            ),
+            OpenApiParameter(
+                "show_theme",
+                type=OpenApiTypes.STR,
+                description="Filter by show theme ID or comma separated list of IDs",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 class PlanetariumDomeViewSet(
