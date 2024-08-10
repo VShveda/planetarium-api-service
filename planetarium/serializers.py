99e60c9e484376d1fdd9c303638ddbb666979412
Viktor Sveda <vinix.ru@gmail.com>
2024-08-10 14:17:25 +0300

    add AstronomyShowListSerializer end AstronomyShowDetailSerializer

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -22,6 +22,24 @@ class AstronomyShowSerializer(serializers.ModelSerializer):
         fields = ("id", "title", "description", "show_theme")
 
 
+class AstronomyShowListSerializer(AstronomyShowSerializer):
+    show_theme = ShowThemeSerializer(
+        many=True, read_only=True, slug_field="name"
+    )
+
+    class Meta:
+        model = AstronomyShow
+        fields = ("id", "title", "show_theme")
+
+
+class AstronomyShowDetailSerializer(AstronomyShowSerializer):
+    show_theme = ShowThemeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = AstronomyShow
+        fields = ("id", "title", "description", "show_theme")
+
+
 class PlanetariumDomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlanetariumDome
